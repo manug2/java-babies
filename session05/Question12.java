586425729a7c6e754d13a6becff141ec11c29767
manu <manugarg2@gmail.com>
2025-12-13 11:03:45 +0800

    session 05 - Quiz

---
+++ session05/Question12.java
@@ -0,0 +1,17 @@
+public class Question12 {
+
+   {
+      int fins = 100;
+   }
+
+   public static void main(String[] args) {
+
+      Question12 answer = new Question12();
+
+      System.out.println(fins);
+      System.out.println(answer.fins);
+
+   }
+
+}
+
